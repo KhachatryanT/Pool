72767acf899a2c1d5814819d0826d5cbcddcf440
KhachatrianTS <KhachatrianTS@quantumart.ru>
2022-12-06 11:09:20 +0300

    add Pool.WebUI with angular

--- Pool.Api/Program.cs
+++ Pool.Api/Program.cs
@@ -1,59 +1,8 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
-using Pool.Api.Application;
-using Pool.Api.Application.Middleware;
-using Pool.Api.Jobs;
-using Pool.CQRS;
-using Pool.DAL;
-using Pool.DevicesController.Crystal;
-using Pool.DevicesController.Fake;
-using Pool.DevicesControllers;
-using Quartz;
+using Pool.Api;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers()
-	.AddJsonOptions(options =>
-	{
-		options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
-	});
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCQRS();
-builder.Services.AddDal();
-builder.Services.AddDevicesControllers(builder.Configuration.GetSection("Pools"));
-//builder.Services.AddCrystalDevicesController();
-builder.Services.AddFakeDevicesController();
-
-builder.Services.AddDbContext<PoolContext>(options=>
-{
-	options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-	options.UseSqlite(builder.Configuration.GetConnectionString("Pool"));
-});
-
-builder.Services.AddQuartz(q =>
-{
-	q.UseMicrosoftDependencyInjectionJobFactory();
-	q.AddJobAndTrigger<MonitoringJob>(builder.Configuration.GetSection("Quartz"));
-});
-builder.Services.AddQuartzHostedService();
+builder.Services.AddApi(builder.Configuration);
 
 var app = builder.Build();
-
-await app.EnsureMigrationAsync<PoolContext>();
-
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UsePoolExceptionHandling();
-
-app.MapDefaultControllerRoute();
-
+await app.ConfigureApiAsync();
 app.Run();
---
+++ Pool.WebUI/Program.cs
@@ -0,0 +1,31 @@
+using Pool.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddApi(builder.Configuration);
+
+var app = builder.Build();
+await app.ConfigureApiAsync();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
