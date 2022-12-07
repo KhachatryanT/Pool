7c4a090318c33ccd352e65746c36a737b1fbf02a
Maksim Tihomirov <maxbeere@yandex.ru>
2022-12-07 07:26:16 +0300

    delete components

---
+++ Pool.WebUI/ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
