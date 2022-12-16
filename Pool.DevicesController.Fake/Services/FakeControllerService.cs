a09f327338da398b8f93160be580a2648c257885
KhachatrianTS <KhachatrianTS@quantumart.ru>
2022-12-16 15:43:53 +0300

    fake devices values: to real random values

--- Pool.DevicesController.Fake/Services/FakeControllerService.cs
+++ Pool.DevicesController.Fake/Services/FakeControllerService.cs
@@ -16,7 +16,27 @@ internal sealed class FakeControllerService : IControllerService
 		{
 			Date = DateTimeOffset.Now,
 			Type = type,
-			Value = new Random(Environment.TickCount).NextDouble()
+			Value = GetRandomValue(type)
 		}).ToArray();
+	}
+
+	private static double GetRandomValue(DeviceType type) =>
+		type switch
+		{
+			DeviceType.Ph => Math.Round(GetRandomValue(6.8, 8), 2),
+			DeviceType.Cl => Math.Round(GetRandomValue(0.2, 1.5), 2),
+			DeviceType.Rx => GetRandomValue(300, 900),
+			DeviceType.Temp => Math.Round(GetRandomValue(24.0, 27.0), 1),
+			_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+		};
+
+	private static double GetRandomValue(double minimum, double maximum)
+	{
+		return new Random(Environment.TickCount).NextDouble() * (maximum - minimum) + minimum;
+	}
+
+	private static int GetRandomValue(int minimum, int maximum)
+	{
+		return new Random(Environment.TickCount).Next(minimum, maximum);
 	}
 }
